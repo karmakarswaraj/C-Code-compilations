40ce7fbdf4a2dd7ba9710cfa3dd8646a5e6207d8
Swaraj Karmakar <72521556+karmakarswaraj@users.noreply.github.com>
2022-03-17 03:51:19 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,15 @@
+//C++ Program to Check Whether Number is Even or Odd
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+    
+  (n % 2 == 0) ? cout << n << " is even." :  cout << n << " is odd.";
+    
+  return 0;
+}
