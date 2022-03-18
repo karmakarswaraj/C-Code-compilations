455e0b9ef36447901619287c6b608d0fb57c8817
Swaraj Karmakar <72521556+karmakarswaraj@users.noreply.github.com>
2022-03-18 14:58:19 +0530

    Add files via upload

---
+++ Volume of a cube.cpp
@@ -0,0 +1,17 @@
+//Calculate the volume of a cube
+
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+    	int sid1;
+    	float volcu;
+		cout << "Calculate the volume of a cube :\n";		
+        cout<<" Input the side of a cube : ";
+    	cin>>sid1;
+    	volcu=(sid1*sid1*sid1);
+        cout<<" The volume of a cube is : "<< volcu << endl;
+        cout << endl;
+        return 0;
+    }
