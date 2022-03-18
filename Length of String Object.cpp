2b96be6173a8a98c820e90c63aba8300d87e60a5
Swaraj Karmakar <72521556+karmakarswaraj@users.noreply.github.com>
2022-03-18 14:57:25 +0530

    Add files via upload

---
+++ Length of String Object.cpp
@@ -0,0 +1,13 @@
+//Length of String Object
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "C++ Programming";
+
+    // you can also use str.length()
+    cout << "String Length = " << str.size();
+
+    return 0;
+}
